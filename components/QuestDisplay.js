96bbe92af7047b1b6e983aa06a3f41387b50d0ba
Mark Graham <markgraham924@gmail.com>
2024-03-18 11:35:36 +0000

    v1.3

--- components/QuestDisplay.js
+++ components/QuestDisplay.js
@@ -1,20 +1,44 @@
 import React from 'react';
 import { ScrollView } from 'react-native';
-import QuestCard from './QuestCard'; // Assuming you have defined QuestCard component
-import questsData from '../assets/quests/quest.json'; // Import the quests data
+import QuestCard from './QuestCard';
+import questsData from '../assets/quests/quest.json';
 
-const QuestsDisplay = () => (
-  <ScrollView>
-    {questsData.map(quest => (
-      <QuestCard
-        key={quest.id}
-        title={quest.title}
-        description={quest.description}
-        points={quest.points}
-        gif={require('../assets/gym-card.gif')}
-      />
-    ))}
-  </ScrollView>
-);
+
+const gymCardGif = require('../assets/gym-card.gif');
+const mentalCardGif = require('../assets/gym-card.gif');
+const perceptionCardGif = require('../assets/gym-card.gif');
+const charismaCardGif = require('../assets/gym-card.gif');
+
+
+const getGifForType = (type) => {
+  switch(type) {
+    case 'exercise': return gymCardGif;
+    case 'mental': return mentalCardGif;
+    case 'perception': return perceptionCardGif;
+    case 'charisma': return charismaCardGif;
+    default: return gymCardGif; 
+  }
+};
+
+const QuestsDisplay = () => {
+  const mergedQuestsData = questsData.map(quest => ({
+    ...quest,
+    gif: getGifForType(quest.type)
+  }));
+
+  return (
+    <ScrollView>
+      {mergedQuestsData.map(quest => (
+        <QuestCard
+          key={quest.id}
+          title={quest.title}
+          description={quest.description}
+          points={quest.points}
+          gif={quest.gif} 
+        />
+      ))}
+    </ScrollView>
+  );
+};
 
 export default QuestsDisplay;
